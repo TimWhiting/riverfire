df509e2d7cf9a6716778bba4d4146df9af8cd23b
Tim Whiting <tim@whitings.org>
2021-12-20 11:23:14 -0700

    upgrade dependencies

--- lib/src/collection.dart
+++ lib/src/collection.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:json_annotation/json_annotation.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:rxdart/rxdart.dart';
 
